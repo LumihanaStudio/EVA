6a3380c1780eadb1446d52d644d47e0c2b5fc877
Junseok Oh <kotohana5706@edcan.kr>
2016-07-22 17:38:56 +0900

    RequestActivity Design Complete

--- app/src/main/java/kr/edcan/eva/RequestActivity.java
+++ app/src/main/java/kr/edcan/eva/RequestActivity.java
@@ -1,13 +1,34 @@
 package kr.edcan.eva;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RequestActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+        setAppbarLayout();
     }
+
+    private void setAppbarLayout() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().setTitle("EVA 요청하기");
+        getSupportActionBar().setElevation(5);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Drawable drawable = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
+        drawable.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(drawable);
+    }
+
 }
